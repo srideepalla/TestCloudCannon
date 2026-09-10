2803be28509343bc8e59b663cf82967dbb99a94d
AvinashKamadri <avinash@flashbacklabs.com>
2026-09-11 00:27:47 +0530

    Register new collection in Astro content config
    Replace tickets collection with new collection so /new/ pages render

--- src/content.config.ts
+++ src/content.config.ts
@@ -119,7 +119,7 @@ const judgesCollection = makeCollection("./src/content/judges");
 const pressCollection = makeCollection("./src/content/press");
 const sponsorsCollection = makeCollection("./src/content/sponsors");
 const tagsCollection = makeCollection("./src/content/tags");
-const ticketsCollection = makeCollection("./src/content/tickets");
+const newCollection = makeCollection("./src/content/new");
 
 export const collections = {
   pages: pagesCollection,
@@ -132,5 +132,5 @@ export const collections = {
   press: pressCollection,
   sponsors: sponsorsCollection,
   tags: tagsCollection,
-  tickets: ticketsCollection,
+  new: newCollection,
 };
